47042f424d1daa514e0cb3cd64ec2d61a88a0f26
mikha-dev <mikha.dev@gmail.com>
2022-12-03 22:37:55 +0300

    added more tests

--- tests/MT4SdkTest.php
+++ tests/MT4SdkTest.php
@@ -4,11 +4,14 @@ namespace D4T\MT4Sdk\Tests;
 
 use D4T\MT4Sdk\Facades\MT4Manager;
 use D4T\MT4Sdk\MT4SdkServiceProvider;
+use D4T\MT4Sdk\Resources\Account;
 use Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class MT4SdkTest extends Orchestra
 {
+
+    private ?Account $account = null;
 
     /**
      * @test
@@ -31,9 +34,32 @@ class MT4SdkTest extends Orchestra
      */
     public function can_create_account()
     {
-        /** @var D4T\MT4Sdk\Resources\Account $account */
-        $account = MT4Manager::createAccount(['name' => 'TestL', 'email' => 'testl@test.com', 'group' => 'demoHFX-USD']);
-        $this->assertEquals('TestL', $account->name);
+        $this->account = MT4Manager::createAccount(['name' => 'TestL', 'email' => 'testl@test.com', 'group' => 'demoHFX-USD']);
+        $this->assertEquals('TestL', $this->account->name);
+    }
+
+    /**
+     * @test
+     */
+    public function can_update_account()
+    {
+        if($this->account) {
+            $this->account = MT4Manager::updateAccount($this->account->login, ['name' => 'TestU']);
+            return $this->assertEquals('TestU', $this->account->name);
+        }
+
+        return $this->assertTrue(false);
+    }
+
+    /**
+     * @test
+     */
+    public function can_delete_account()
+    {
+        if($this->account)
+            return $this->assertTrue(MT4Manager::deleteAccount($this->account->login));
+
+        return $this->assertTrue(false);
     }
 
     public function setUp(): void
