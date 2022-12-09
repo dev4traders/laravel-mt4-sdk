536bc006c51cdf49e52945a318d502a6c85f1dd0
mikha-dev <mikha.dev@gmail.com>
2022-12-09 12:17:08 +0300

    added facade accessors

--- src/Facades/MT4Manager.php
+++ src/Facades/MT4Manager.php
@@ -5,7 +5,17 @@ namespace D4T\MT4Sdk\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @see \D4T\MT4Sdk\MT4Sdk
+ * @see D4T\MT4Sdk\Manager
+ * @method static string apiToken()
+ * @method static string endpoint()
+ * @method static bool ping()
+ * @method static array listAccountLogins()
+ * @method static Account getAccount(int $login)
+ * @method static Account createAccount(array $data)
+ * @method static Account updateAccount(int $login, array $data):
+ * @method static bool deleteAccount(int $login)
+ * @method static Trade openTrade(int $accountLogin, array $data)
+ * @method static void closeTrade(int $ticket)
  */
 class MT4Manager extends Facade
 {
